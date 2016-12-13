51aeff56e16e20be35491a1ed88097a04e0e769f
Michael Malak <michael.k.malak@gmail.com>
2016-12-13 23:56:53 +0200

    Update main.asm

--- main.asm
+++ main.asm
@@ -1,10 +1,9 @@
 ;;=============================================================================;;   
-;;                    Shooting rockets Game                                    ;;
+;;                 Simple Rocket Shooting Game                                 ;;
 ;;                 Tested on DOSBox and emu8086                                ;;
 ;;=============================================================================;;
 ;;                  Created by: Michael Malak                        	       ;;
 ;;=============================================================================;;
-
 
 Print MACRO row, column, color 
    push ax
@@ -80,14 +79,16 @@ PrintText Macro row , column , text
    push bx
    push cx
    push dx   
-	mov ah,2
-	mov bh,0
-	mov dl,column
-	mov dh,row
-	int 10h
-	mov ah, 9
-    mov dx, offset text
-    int 21h
+   
+   mov ah,2
+   mov bh,0
+   mov dl,column
+   mov dh,row
+   int 10h
+   mov ah, 9
+   mov dx, offset text
+   int 21h
+   
    pop dx
    pop cx
    pop bx
@@ -96,7 +97,6 @@ ENDM PrintText
 
 ClearScreen MACRO
         
-    ;Clear the screen    
     mov ax, 0600h  ;al=0 => Clear
     mov bh, 07     ;bh=07 => Normal Attributes              
     mov cx, 0      ;From (cl=column, ch=row)
@@ -126,26 +126,25 @@ Delete Macro row, column
 ENDM Delete
 
 Delay  Macro Seconds, MilliSeconds
-    
     push ax
     push bx
     push cx
     push dx 
-	push ds
+    push ds
 
     mov cx, Seconds		;Cx,Dx : number of microseconds to wait
     mov dx, MilliSeconds
     mov ah, 86h
     int 15h
 	
-	pop ds
-	pop dx
+    pop ds
+    pop dx
     pop cx
     pop bx
     pop ax
-	
 ENDM Delay 
 
+;=========================================
 .MODEL SMALL
 .STACK 64    
 .DATA 
@@ -172,7 +171,7 @@ GameoverScreen			 db '          ________________________________________________
 	db '$',0ah,0dh
 RocketColLeft          db       ? 										 
 RocketColRight         db       ? 
-RocketColCenter	       db		?
+RocketColCenter	       db	?
 
 
 RocketRow              db       15    
@@ -183,28 +182,30 @@ ShooterCol             db      40
    
 ShotRow                db      ?
 ShotCol                db      ?
-ShotStatus             db      0    		;1 means there exist a displayed shot, 0 otherwise
+ShotStatus             db      0    						;1 means there exist a displayed shot, 0 otherwise
 
 lifes                  db      6
 Misses                 db      0
 Hits                   db      0
-PlayerName			   db 15, ?,  15 dup('$')
-AskPlayerName		   db 'Enter your name: ','$'
-Disp_Hits			   db 'Score: 00','$'
-Disp_lifes			   db 'lifes: ?','$'
-GameTitle			   db ' >>  Shooting rockets Game  << ','$'
-FinalScoreString	   db ' your final score is ?','$'
-RocketDirection		   db 0						;0=Left, 1=Right
-EasyMode			   db 'Easy Mode','$'
-HardMode			   db 'Hard Mode','$'
-ExtremeMode			   db 'Extreme Mode','$'
+PlayerName	       db      15, ?,  15 dup('$')
+AskPlayerName	       db      'Enter your name: ','$'
+Disp_Hits	       db      'Score: 00','$'
+Disp_lifes             db      'lifes: ?','$'
+GameTitle	       db      ' >>  Shooting rockets Game  << ','$'
+FinalScoreString       db      ' your final score is ?','$'
+RocketDirection	       db      0						;0=Left, 1=Right
+EasyMode	       db      'Easy Mode','$'
+HardMode	       db      'Hard Mode','$'
+ExtremeMode	       db      'Extreme Mode','$'
 ;==================================================
+
 .CODE   
 MAIN    PROC FAR  
     mov ax, @DATA
     mov ds, ax  
+    
   ClearScreen
-  Call StartMenu
+  call StartMenu
   ClearScreen
   call DrawInterface
   call ResetRocket
@@ -216,6 +217,7 @@ MAIN    PROC FAR
    jz moveRocketRight
    call RocketMoveLeft
    jmp AfterRocketMove
+   
    moveRocketRight:
    call RocketMoveRight
    
@@ -223,24 +225,27 @@ MAIN    PROC FAR
    cmp ShotStatus, 1
    jnz NoShotExist
    call CheckShotStatus			;I'll see if the shotStatus alter to 0
+   
    cmp ShotStatus, 1
    jnz NoShotExist
    call MoveShot
    PrintShooter ShooterCol		;since the shot deletes the shooter at the beginning
+   
    NoShotExist:       
-    mov ah,1h
-    int 16h             ;ZF=1 when a key is pressed                        
-    jz NokeyPress
-      call KeyisPressed
-    NokeyPress:
-	call Difficulty
-	EndOfMainLoop:
-  jmp MainLoop
-
-hlt
-    
-    
+   mov ah,1h
+   int 16h             			;ZF=1 when a key is pressed                        
+   jz NokeyPress
+   call KeyisPressed
+   
+   NokeyPress:
+   call Difficulty
+   
+   EndOfMainLoop:
+   jmp MainLoop
+   hlt
 MAIN        ENDP 
+
+;==================================================
 UpdateStrings Proc  
 	 push ax
 	 
@@ -249,86 +254,87 @@ UpdateStrings Proc
 	 mov Disp_Hits[8], al
 	 mov FinalScoreString[21], al
 		
-    mov ah,lifes
-    add ah, 30h
-    mov Disp_lifes[7], ah
+    	mov ah,lifes
+    	add ah, 30h
+   	 mov Disp_lifes[7], ah
 	
 	PrintText 1 , 60 , Disp_Hits
 	PrintText 1 , 70 , Disp_lifes	
 
 	pop ax
 	ret             
-    
 UpdateStrings ENDP 
 
+;==================================================
 RocketMoveLeft Proc   
     dec RocketColLeft
     Print   RocketRow ,RocketColLeft, RocketColor 
     Delete RocketRow, RocketColRight     
     dec RocketColRight  
-	dec RocketColCenter
+    dec RocketColCenter
 	
     cmp RocketColLeft ,0   
     Jnz endOfRocketMoveLeft 
     call DeleteRocket
-	call ResetRocket
+    call ResetRocket
     endOfRocketMoveLeft: ret              
 RocketMoveLeft ENDP 
 
+;==================================================
 RocketMoveRight Proc   
     inc RocketColRight
     Print   RocketRow ,RocketColRight, RocketColor 
     Delete RocketRow, RocketColLeft     
     inc RocketColleft 
-	inc RocketColCenter
+    inc RocketColCenter
 	
     cmp RocketColRight ,80   
     Jnz endOfRocketMoveRight 
     call DeleteRocket
-	call ResetRocket
+    call ResetRocket
     endOfRocketMoveRight: ret              
 RocketMoveRight ENDP 
 
+;==================================================
 KeyisPressed  Proc 
     mov ah,0
     int 16h
 
     cmp ah,4bh                            ;Move Shooter Left if left button is pressed
     jnz NotLeftKey
-		call MoveShooterLeft 
-	jmp EndofKeyisPressed
+    call MoveShooterLeft 
+    jmp EndofKeyisPressed
 	
     NotLeftKey:
     cmp ah,4dh					
-    jnz NotRightKey						  ;Move Shooter Right if Right button is pressed
-		call MoveShooterRight
-	jmp EndofKeyisPressed
+    jnz NotRightKey			 ;Move Shooter Right if Right button is pressed
+    call MoveShooterRight
+    jmp EndofKeyisPressed
 	
     NotRightKey:
-	cmp ah,1H                 		      ;Esc to exit the game
-    ;try: cmp ax, 011bh
-	
-	Jnz NotESCKey
-		call Gameover 
+    cmp ah,1H                            ;Esc to exit the game
+
+     Jnz NotESCKey
+     call Gameover 
 		
-	NotESCKey:
+    NotESCKey:
     cmp ah,39h                            ;go spaceKey if up button is pressed
-    ;try:cmp ax,3920
 	
-	jnz EndofKeyisPressed
+    jnz EndofKeyisPressed
     cmp ShotStatus, 1
     jz EndofKeyisPressed
-            mov al,1                      ;intialize a new shot
-            mov ShotStatus,1 
-            mov al, ShooterCol
-            mov ShotCol, al
-            mov al, 24					 ;it will be decremented in the new MainLoop
-            mov ShotRow,al 
+    mov al,1                      	  ;intialize a new shot
+    mov ShotStatus,1 
+    mov al, ShooterCol
+    mov ShotCol, al
+    mov al, 24				  ;it will be decremented in the new MainLoop
+    mov ShotRow,al 
 			
     EndofKeyisPressed:
     ret
 KeyisPressed  ENDP 
 
+;==================================================
 MoveShooterLeft  Proc  
      cmp ShooterCol, 0
      JZ NoMoveLeft
@@ -341,6 +347,7 @@ MoveShooterLeft  Proc
     ret
 MoveShooterLeft  ENDP 
 
+;==================================================
 MoveShooterRight  Proc 
      cmp ShooterCol, 79
      JZ NoMoveRight
@@ -353,6 +360,7 @@ MoveShooterRight  Proc
     ret
 MoveShooterRight  ENDP 
 
+;==================================================
 MoveShot  Proc 
     dec ShotRow
     PrintShot ShotRow,ShotCol 
@@ -362,11 +370,12 @@ MoveShot  Proc
     ret
 MoveShot  ENDP 
 
+;==================================================
 CheckShotStatus  Proc
     push ax
 	
     mov ah,RocketRow
-    inc ah              ;Checking the row I {WILL} draw the shot in if occupied by a rocket
+    inc ah              			;Checking the row I {WILL} draw the shot in if occupied by a rocket
     cmp ah, ShotRow  
     JNZ CheckEndRange 
 						;Check if it was a hit
@@ -390,7 +399,8 @@ CheckShotStatus  Proc
 		cmp al, ah
         JZ Hit
 		
-    CheckEndRange:
+   ;==================================================
+   CheckEndRange:
 	 cmp ShotRow, 2			;It stops while printed on the number of row I put here
 	 jnz noChange			
 	 dec Lifes
@@ -411,6 +421,7 @@ CheckShotStatus  Proc
     ret    
 CheckShotStatus ENDP 
 
+;==================================================
 DeleteRocket Proc
 	 Delete RocketRow, RocketColLeft
 	 Delete RocketRow, RocketColCenter
@@ -418,6 +429,7 @@ DeleteRocket Proc
 	ret
 DeleteRocket ENDP
 
+;==================================================
 RandomiseRocketRow Proc    
    push ax
    push bx
@@ -426,7 +438,7 @@ RandomiseRocketRow Proc
    
    ; Range of row= [5,24]
    mov ah, 2ch                
-   int 21h                      ; get system time where DH = second   Dl=MilliSeconds
+   int 21h                      		; get system time where DH = second   Dl=MilliSeconds
    xor ax, ax
    mov al, dl
    mov bl, 20					; That limits the remainder to be [0,19]
@@ -436,7 +448,7 @@ RandomiseRocketRow Proc
    
    ;Change the color of rocket
    NotBlack:
-   add RocketColor ,10h			;Add one to background color
+   add RocketColor ,10h				;Add one to background color
    cmp RocketColor ,00h
    jz NotBlack
         
@@ -447,6 +459,7 @@ RandomiseRocketRow Proc
    ret  
 RandomiseRocketRow ENDP 
 
+;==================================================
 ResetRocket Proc
     call RandomiseRocketDirection
 	call RandomiseRocketRow
@@ -467,6 +480,7 @@ ResetRocket Proc
     ret 
 ResetRocket ENDP 
 
+;==================================================
 RandomiseRocketDirection Proc
    push ax
    push bx
@@ -489,6 +503,7 @@ RandomiseRocketDirection Proc
 	ret
 RandomiseRocketDirection ENDP
 
+;==================================================
 ResetShot Proc
 	 delete ShotRow, ShotCol  
      mov al,0          
@@ -496,28 +511,27 @@ ResetShot Proc
 	ret
 ResetShot ENDP 
 
+;==================================================
 StartMenu Proc
     
-    push ax
-    push bx
-    push cx
-    push dx
-    push ds 
-    
+	push ax
+	push bx
+	push cx
+	push dx
+	push ds 
+
 	ClearScreen
-    
 	LoopOnName:
 	PrintText 8,8,AskPlayerName
-	
+
 	;Receive player name from the user
-		mov ah, 0Ah
-		mov dx, offset PlayerName
-		int 21h
-	
-	
+	mov ah, 0Ah
+	mov dx, offset PlayerName
+	int 21h
+
 	cmp PlayerName[1], 0	;Check that input is not empty
 	jz LoopOnName
-	
+
 	;Checks on the first letter to ensure that it's either a capital letter or a small letter
 	cmp PlayerName[2], 40h
 	jbe LoopOnName
@@ -529,44 +543,42 @@ StartMenu Proc
 	anotherCheck:
 	cmp PlayerName[2], 5Ah
 	ja	LoopOnName
-	
+
 	ExitLoopOnName:
 	ClearScreen
 	PrintText 1,1,StartScreen	
-	
+
 	;hide curser
 	 mov ah,01h
 	  ;If bit 5 of CH is set, that often means "Hide cursor". So CX=2607h is an invisible cursor.
 	 mov cx,2607h 
 	 int 10h
-	 
-    checkforinput:
-    mov AH,0            		 
-    int 16H 
-    
-    cmp al,13              		     ;Enter to Start Game   
-    JE StartTheGame
-    
-    cmp ah,1H                 		 ;Esc to exit the game
-    JE ExitMenu
-    JNE checkforinput
-    
-	
-	 
-    ExitMenu:
-        mov ah,4CH
-        int 21H
-    
-        
-    StartTheGame: 
-        pop ds
-        pop dx
-        pop cx
-        pop bx
-        pop ax 
-        RET
+
+	checkforinput:
+	mov AH,0            		 
+	int 16H 
+
+	cmp al,13              		     ;Enter to Start Game   
+	JE StartTheGame
+
+	cmp ah,1H                 		 ;Esc to exit the game
+	JE ExitMenu
+	JNE checkforinput
+
+	ExitMenu:
+	mov ah,4CH
+	int 21H
+
+	StartTheGame: 
+	pop ds
+	pop dx
+	pop cx
+	pop bx
+	pop ax 
+	RET
 StartMenu ENDP
 
+;==================================================
 DrawInterface	Proc
 	
 	push ax
@@ -594,10 +606,10 @@ DrawInterface	Proc
 	pop dx
 	pop cx
 	pop ax
-	
 	RET
 DrawInterface	ENDP
 
+;==================================================
  Gameover Proc 
  ClearScreen
 
@@ -610,7 +622,8 @@ DrawInterface	ENDP
     int 21H 
     ret
  Gameover ENDP   
- 
+
+;==================================================
 Difficulty Proc
 	
 	cmp Hits, 5
@@ -629,5 +642,5 @@ Difficulty Proc
 	EndDifficulty:
 	ret
 Difficulty ENDP
-
+;==================================================
 END MAIN    
